b8b2cb82df997e6c7f60433c92e061738aba5c56
winterbreaker22 <joejamesdev@hotmail.com>
2025-01-14 20:20:45 -0800

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,16 +1,18 @@
-import os
+import subprocess
 
-# Define paths
-PIPELINE_CONFIG_PATH = "/model/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/pipeline.config"
-MODEL_DIR = "/model/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8"
-NUM_TRAIN_STEPS = 10000
+def train_model():
+    pipeline_config_path = "model/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/pipeline.config"
+    model_dir = "model/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/checkpoint"
 
-# Construct training command
-train_command = (f"python models/research/object_detection/model_main_tf2.py "
-                 f"--pipeline_config_path={PIPELINE_CONFIG_PATH} "
-                 f"--model_dir={MODEL_DIR} "
-                 f"--num_train_steps={NUM_TRAIN_STEPS} "
-                 f"--alsologtostderr")
+    command = [
+        "python", 
+        "models/research/object_detection/model_main_tf2.py",
+        "--pipeline_config_path", pipeline_config_path,
+        "--model_dir", model_dir,
+        "--num_train_steps", "10000"
+    ]
 
-# Run training command
-os.system(train_command)
+    subprocess.run(command)
+
+if __name__ == "__main__":
+    train_model()
