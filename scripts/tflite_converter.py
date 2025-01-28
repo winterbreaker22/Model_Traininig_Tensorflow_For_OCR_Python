1aa3a882d59b14c63be916da5eb423da55257b3d
winterbreaker22 <joejamesdev@hotmail.com>
2025-01-27 19:01:36 -0800

    Update tflite_converter.py

--- scripts/tflite_converter.py
+++ scripts/tflite_converter.py
@@ -1,10 +1,12 @@
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_saved_model("exported_model/saved_model")
+saved_model_dir = "exported_model/saved_model"
+
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
 
 converter.target_spec.supported_ops = [
-    tf.lite.OpsSet.TFLITE_BUILTINS,   
-    tf.lite.OpsSet.SELECT_TF_OPS     
+    tf.lite.OpsSet.TFLITE_BUILTINS,  
+    tf.lite.OpsSet.SELECT_TF_OPS  
 ]
 
 try:
@@ -13,11 +15,6 @@ try:
     
     with open("exported_model/model.tflite", "wb") as f:
         f.write(tflite_model)
-
 except Exception as e:
     print(f"Error during conversion: {e}")
 
-try:
-    tf.lite.experimental.Analyzer.analyze(model_path='exported_model/model.tflite')
-except Exception as e:
-    print(f"Error during model analysis: {e}")
